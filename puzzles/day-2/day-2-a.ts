2a01c630f9ad11af314abc7f07b30fee2dc751ff
Vinit Neogi <20491952+vneogi199@users.noreply.github.com>
2023-12-08 18:04:01 +0530

    solve day 2 of advent of code

--- puzzles/day-2/day-2-a.ts
+++ puzzles/day-2/day-2-a.ts
@@ -1,9 +1,33 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const maxPossible = {
+  red: 12,
+  green: 13,
+  blue: 14,
+};
 export async function day2a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const results = data
+    .map((row) => {
+      if (!row.length) {
+        return 0;
+      }
+      const [gameStr, game] = row.split(': ');
+      const gameNum = gameStr.split(' ')[1];
+      const cubes = game.split('; ');
+      const isPossible = cubes.every((cube) => {
+        const colorCubes = cube.split(', ');
+        return colorCubes.every((c) => {
+          const [count, color] = c.split(' ');
+          return Number(count) <= maxPossible[color];
+        });
+      });
+      return isPossible ? Number(gameNum) : 0;
+    })
+    .reduce((acc, curr) => acc + curr, 0);
+  return results;
 }
 
 const answer = await day2a();
--- puzzles/day-2/day-2-b.ts
+++ puzzles/day-2/day-2-b.ts
@@ -3,7 +3,33 @@ import chalk from 'chalk';
 
 export async function day2b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const results = data
+    .map((row) => {
+      if (!row.length) {
+        return 0;
+      }
+      const game = row.split(': ')[1];
+      const minCountMap = {
+        red: 0,
+        green: 0,
+        blue: 0,
+      };
+      const cubes = game.split('; ');
+      cubes.forEach((cube) => {
+        const colorCubes = cube.split(', ');
+        colorCubes.forEach((c) => {
+          const [count, color] = c.split(' ');
+          const countNum = Number(count);
+          if (countNum > minCountMap[color]) {
+            minCountMap[color] = countNum;
+          }
+        });
+      });
+      return Object.values(minCountMap).reduce((acc, curr) => acc * curr, 1);
+    })
+    .reduce((acc, curr) => acc + curr, 0);
+  return results;
 }
 
 const answer = await day2b();
